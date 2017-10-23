bcd6b9e79a207be90dfc237370abb1f7d2604fda
Suzichen <suzichenapk@gmail.com>
2017-10-23 11:19:37 +0800

    修改 indexof为indexOf

--- Ajax.js
+++ Ajax.js
@@ -25,7 +25,7 @@ var _ = (function() {
                 var value = data[name].toString();
                 name = encodeURIComponent(name);
                 value = encodeURIComponent(value);
-                url += (url.indexof("?") == -1 ? "?" : "&");
+                url += (url.indexOf("?") == -1 ? "?" : "&");
                 url += name + "=" + value;
             }
             return url;
